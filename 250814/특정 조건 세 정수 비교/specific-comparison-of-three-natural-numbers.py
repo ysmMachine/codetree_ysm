1d5e16a0089d24e5c9d5e475a2dae4bf25a8aca4
ysm <seoowol55@gmail.com>
2025-08-14 08:40:40 +0900

    Title: 특정 조건 세 정수 비교, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250814/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 250814/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,11 +1,11 @@
 li = list(map(int, input().split()))
 
 if li[0] == min(li):
-    print(1)
+    print(1, end=" ")
 else:
-    print(0)
+    print(0, end=" ")
 
 if li[0] == li[1] == li[2]:
-    print(1)
+    print(1, end=" ")
 else:
-    print(0)
+    print(0, end=" ")
