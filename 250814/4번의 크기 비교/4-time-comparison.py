befa491b88fe3a16bd785bed15701dfd934085a6
ysm <seoowol55@gmail.com>
2025-08-14 08:34:08 +0900

    Title: 4번의 크기 비교, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250814/4번의 크기 비교/4-time-comparison.py
+++ 250814/4번의 크기 비교/4-time-comparison.py
@@ -1,7 +1,14 @@
 a = int(input())
 b, c, d, e = list(map(int, input().split()))
 
-if a > b: print(1) else: print(0)
-if a > c: print(1) else: print(0)
-if a > d: print(1) else: print(0)
-if a > e: print(1) else: print(0)
+if a > b: print(1) 
+else: print(0)
+
+if a > c: print(1) 
+else: print(0)
+
+if a > d: print(1) 
+else: print(0)
+
+if a > e: print(1) 
+else: print(0)
