ed02f9e973ca1e294e0829d307b5c4619ad201c0
ysm <seoowol55@gmail.com>
2025-08-14 22:42:19 +0900

    Title: 함수를 이용한 소수 판별, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250814/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250814/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -3,8 +3,8 @@ a, b = map(int, input().split())
 # Please write your code here.
 def is_prime(num):
     for i in range(2, num//2+1):
-        if num % i == 0: return True
-    return False
+        if num % i == 0: return False
+    return True
 
 ans = 0
 for i in range(a, b+1):
