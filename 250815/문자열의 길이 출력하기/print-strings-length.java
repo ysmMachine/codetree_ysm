b3edfd10e15f0b28daa782be537b5a8aeae992ad
ysm <seoowol55@gmail.com>
2025-08-15 11:28:35 +0900

    Title: 문자열의 길이 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 250815/문자열의 길이 출력하기/print-strings-length.java
+++ 250815/문자열의 길이 출력하기/print-strings-length.java
@@ -2,13 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-
         String str1 = sc.next();
         String str2 = sc.next();
-
-        System.println.out(str1.length() + str2.length());
-        
+        System.out.println(str1.length() + str2.length());
     }
 }
