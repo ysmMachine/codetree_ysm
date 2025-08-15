e859a8f1d1bfc3c71ed4db6123bce7876e6865d9
ysm <seoowol55@gmail.com>
2025-08-15 12:13:36 +0900

    Title: 공백을 기준으로 출력, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 250815/공백을 기준으로 출력/output-based-on-space.java
+++ 250815/공백을 기준으로 출력/output-based-on-space.java
@@ -7,17 +7,17 @@ public class Main {
         String s1 = sc.nextLine();
         String s2 = sc.nextLine();
 
-        StirngBuilder sb = new StirngBuilder();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != ' ') sb += s1.charAt(i);
+            if (s1.charAt(i) != ' ') sb.append(s1.charAt(i));
         }
         
         for (int i = 0; i < s2.length(); i++) {
-            if (s2.charAt(i) != ' ') sb += s2.charAt(i);
+            if (s2.charAt(i) != ' ') sb.append(s2.charAt(i));
         }
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
 
     }
 }
