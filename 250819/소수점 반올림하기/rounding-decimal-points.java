49a5aa188c6f551f4299cbe140488f4a80a67245
ysm <seoowol55@gmail.com>
2025-08-19 07:37:39 +0900

    Title: 소수점 반올림하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250819/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double a = 25.352;
+        System.out.printf("%.1f", a);
+    }
+}
