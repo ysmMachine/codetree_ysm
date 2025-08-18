047091bcf70e718262be9956af9a0173962525ce
ysm <seoowol55@gmail.com>
2025-08-19 08:11:56 +0900

    Title: 정수 복사, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250819/정수 복사/copy-integer.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 4;
+        b = a;
+        System.out.println(a + " " + b);
+        System.out.println(b * b);
+    }
+}
