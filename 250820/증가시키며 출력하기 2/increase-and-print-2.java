79431de7f00959ecb6c74cea7d7c883979a5d42a
ysm <seoowol55@gmail.com>
2025-08-20 04:41:36 +0900

    Title: 증가시키며 출력하기 2, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250820/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int num = 10;
+        while (num <= 26) {
+            System.out.print(num + " ");
+            num++;
+        }
+    }
+}
