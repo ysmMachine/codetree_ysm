1b2ece5e484904b7f5db2e332022dcb2001ea356
ysm <seoowol55@gmail.com>
2025-08-20 02:54:48 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250820/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+
+        for (int i = n; i < 101; i++) System.out.print(i + " ");
+    }
+}
