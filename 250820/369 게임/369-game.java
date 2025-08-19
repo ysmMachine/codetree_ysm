7979374425bbb057d8c909ae9c1331096962a0da
ysm <seoowol55@gmail.com>
2025-08-20 08:31:12 +0900

    Title: 369 게임, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250820/369 게임/369-game.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isThree(int num) {
+        while (num != 0) {
+            if (num % 10 == 3) return true;
+            num /= 10;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || isThree(i)) System.out.print(0 + " ");
+            else System.out.print(i + " ");
+        }
+    }
+}
