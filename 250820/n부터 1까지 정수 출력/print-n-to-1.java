3b479cfcc386a9eef8cc5353b32906733777c1f2
ysm <seoowol55@gmail.com>
2025-08-20 04:55:42 +0900

    Title: n부터 1까지 정수 출력, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250820/n부터 1까지 정수 출력/print-n-to-1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n >= 1) {
+            System.out.print(n + " ");
+            n--;
+        }
+        
+    }
+}
