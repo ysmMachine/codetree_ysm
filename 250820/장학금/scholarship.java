d3c2bb3ca5c97fd0aec4c05b226aa3c61b2bbba3
ysm <seoowol55@gmail.com>
2025-08-20 02:11:35 +0900

    Title: 장학금, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250820/장학금/scholarship.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a >= 90) {
+            if (b >= 95) System.out.println(100000);
+            else if (b >= 90) System.out.println(50000);
+        } else { 
+            System.out.println(0);
+        }
+    }
+}
