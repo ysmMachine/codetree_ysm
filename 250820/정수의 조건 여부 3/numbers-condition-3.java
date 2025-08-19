49e3c8eecc8e23b49d352d6afdf34131634bbf1f
ysm <seoowol55@gmail.com>
2025-08-20 01:58:30 +0900

    Title: 정수의 조건 여부 3, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250820/정수의 조건 여부 3/numbers-condition-3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if (a % 13 == 0 || a % 19 == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
