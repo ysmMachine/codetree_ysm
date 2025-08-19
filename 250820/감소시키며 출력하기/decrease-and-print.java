64101945eab37e9bc9ddf6c9e440f7e93acaef52
ysm <seoowol55@gmail.com>
2025-08-20 03:05:00 +0900

    Title: 감소시키며 출력하기, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250820/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int i = 17; i >= 5; i--) System.out.print(i + " ");
+    }
+}
