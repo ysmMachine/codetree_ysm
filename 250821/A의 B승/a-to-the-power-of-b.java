87296235cf29191638404eb900a1a1b629cf0bf6
ysm <seoowol55@gmail.com>
2025-08-21 17:59:02 +0900

    Title: A의 B승, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250821/A의 B승/a-to-the-power-of-b.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int prod = 1;
+        for (int i = 1; i <= b; i++) 
+            prod *= a;
+
+        System.out.println(prod);
+    }
+}
