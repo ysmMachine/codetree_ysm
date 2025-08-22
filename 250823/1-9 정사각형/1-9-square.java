11a8efa495d789fc89c7f36c7f0814690305b0cd
ysm <seoowol55@gmail.com>
2025-08-23 08:11:19 +0900

    Title: 1-9 정사각형, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250823/1-9 정사각형/1-9-square.java
+++ 250823/1-9 정사각형/1-9-square.java
@@ -6,13 +6,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        int cnt = 1;
 
         for (int i = 1; i <= n*n; i++) {
             if (i % n == 0) {
-                System.out.println(i);
+                System.out.println(cnt++);
                 continue;
             }
-            System.out.print(i + "");
+            System.out.print(cnt++ + "");
+
+            if (cnt > 9) cnt = 1;
         }        
     }
 }
